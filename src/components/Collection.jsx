074e0ae1241ce564dfecf37cc7e5a9c02261f3e2
Aravindh Sridharan <mail@aravindh.io>
2020-06-08 14:15:45 +0530

    Added dummy pages for components

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box } from '@chakra-ui/core'
 import List from './List'
 const CollectionViewMapping = {
-  "list":List
+  list: List
 }
 
 export default ({ data, config, view }) => {
